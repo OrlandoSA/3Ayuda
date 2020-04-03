543c78bde0c4d53b9a561040ad6170432df52268
MiguelS <mbeltran164908@alumno.itson.edu.mx>
2020-04-03 13:33:18 -0700

    Corregi algunas cosas que eran diferente al diseño

--- app/src/main/java/Ayuda/a3ayuda/Registro.kt
+++ app/src/main/java/Ayuda/a3ayuda/Registro.kt
@@ -1,12 +1,19 @@
 package Ayuda.a3ayuda
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_registro.*
 
 class Registro : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registro)
+
+        btn_Acceder.setOnClickListener {
+            var intent = Intent(this, Perfil::class.java)
+            startActivity(intent)
+        }
     }
 }
